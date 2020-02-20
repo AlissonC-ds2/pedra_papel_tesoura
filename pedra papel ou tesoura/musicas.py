9caf211f6287691f2837f285ddb5407eb2766cb8
AlissonC-ds2 <61253863+AlissonC-ds2@users.noreply.github.com>
2020-02-20 02:11:55 -0300

    Add files via upload
    Jogo de pedra papel ou tesoura usando multithreading e executando scripts externos.

---
+++ pedra papel ou tesoura/musicas.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+##Transformando as musicas em funçoes para rodar no main script
+def vitoria():
+    audio = AudioSegment.from_mp3('ganhou.mp3')
+    corte = audio[:4500]
+    play(corte)
+
+def empate():
+    audio2 = AudioSegment.from_wav('empate1.wav')
+    corte1 = audio2[:4500]
+    play(corte1)
+
+def perdeu():
+    audio3 = AudioSegment.from_mp3('perdeu.mp3')
+    play(audio3)
+
